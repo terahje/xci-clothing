280bbe8ed7fe4f4d260f9fc9098149b4947fa986
terahje <terahjequinn@gmail.com>
2022-05-13 08:37:04 -0700

    working on adding user with email and password

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -27,17 +28,23 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async(userAuth) => {
+export const createUserDocumentFromAuth = async(
+    userAuth,
+    additionalInformation = {}
+    ) => {
+    if(!userAuth) return;
+    
     const userDocRef = doc(db, 'users', userAuth.uid )
 
     console.log(userDocRef);
@@ -52,8 +59,9 @@ export const createUserDocumentFromAuth = async(userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
-            })
+                createdAt,
+                ...additionalInformation,
+            });
         } catch (error) {
             console.log('error creating the user', error.message);   
         }    
@@ -61,3 +69,9 @@ export const createUserDocumentFromAuth = async(userAuth) => {
 
     return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return; 
+
+    createUserWithEmailAndPassword(auth, email, password)
+}
